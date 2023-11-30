30d2c129aae074853eba65d72348a201253e5c3d
Matthew Nelson <matthewbuzz@gmail.com>
2023-11-30 16:08:07 -0700

    Added functionality to get the function set up

--- single_elimination.py
+++ single_elimination.py
@@ -1,13 +1,69 @@
-print("Input all players' names. Once you have inputed all the names, type 'done'")
+import random
+import time
 
-players = {}
-typed = input("Player: ")
+def evenNumPlayers(players):
+    round = []
+    while len(players) > 0:
+        print(players)
+        matchup = []
+        player1Index = random.randint(0, len(players)-1)
+        matchup.append(players[player1Index])
+        players.pop(player1Index)
+        player2Index = random.randint(0, len(players)-1)
+        matchup.append(players[player2Index])
+        players.pop(player2Index)
+        round.append(matchup)
+    return round
 
-while typed != 'done':
-    if typed in players:
-        print("Player already exists")
+
+
+def createBracket (players):
+    if len(players) % 2 == 0:
+        matchups = evenNumPlayers(players)
+
+    # return oddNumPlayers
+    print("Here are the matchups for this round:")
+    for matchup in matchups:
+        print(f"{matchup[0]} vs. {matchup[1]}")
+    return matchups
+
+def playRound(matchups):
+    ready = input("Ready to play? ")
+    if ready == 'Yes':
+        matchupNum = 1
+        for matchup in matchups:
+            input(f"Who won {matchup[0]} vs. {matchup[1]}? ")
+
+    elif ready == 'Cancel':
+        exit
     else:
-        players[typed] = [0]
-    typed = input("Player: ")
+        playRound(matchups)
 
-print(players)
+
+
+
+def start():
+    print("Input all players' names. Once you have inputed all the names, type 'done'")
+
+    typed = input("Player: ")
+    players = []
+
+    while typed != 'done':
+        if typed in players:
+            print("Player already exists")
+        else:
+            players.append(typed)
+        typed = input("Player: ")
+
+    print("Players: ")
+    time.sleep(0.5)
+    for player in players:
+        print("-" + player)
+        time.sleep(0.5)
+
+    print("Creating bracket...")
+    time.sleep(0.5)
+    matchups = createBracket(players)
+    playRound(matchups)
+
+start()
